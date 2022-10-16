a8b2605052523df89b5b4cf085d12f16367b4ee4
Musa Eren Tanrıöver <93136276+RexelisTakenn@users.noreply.github.com>
2022-10-16 18:09:59 +0300

    Update SinifiGecmeDurumu.java

--- SinifiGecmeDurumu.java
+++ SinifiGecmeDurumu.java
@@ -8,16 +8,12 @@ public class SinifiGecmeDurumu {
 
         System.out.print("Matematik Notunuzu Giriniz = ");
         int matematik = input.nextInt();
-
         System.out.print("Fizik Notunuzu Giriniz = ");
         int fizik = input.nextInt();
-
         System.out.print("Türkçe Notunuzu Giriniz = ");
         int türkce = input.nextInt();
-
         System.out.print("Kimya Notunuzu Giriniz = ");
         int kimya = input.nextInt();
-
         System.out.print("Müzik Notunuzu Giriniz = ");
         int muzik = input.nextInt();
 
