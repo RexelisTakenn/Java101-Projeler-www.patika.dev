c2b09d56b9cbf03ebbe9b667e5698748a7f3f64e
Musa Eren Tanrıöver <93136276+musa-532123@users.noreply.github.com>
2023-03-06 10:28:25 +0300

    Add files via upload

---
+++ DeseneGoreMetotOlusturanProgram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DeseneGoreMetotOlusturanProgram {
+
+    static void figure(int a) {
+        System.out.print(a + " ");
+        if (a <= 0) {
+            return;
+        }
+        figure(a - 5);
+        System.out.print(a + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("N sayısı : ");
+        int n = input.nextInt();
+        System.out.print("Çıktısı : ");
+
+        figure(n);
+
+    }
+}
